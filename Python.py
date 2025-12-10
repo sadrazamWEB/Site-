54ecba3bf4364f69ede9d05bab4185847e08285c
sadrazam404 <sopatik98fd@gmail.com>
2025-12-10 23:17:39 +0300

    Modify home route to use render_template
    Updated home route to render an HTML template instead of returning a string.

--- Python.py
+++ Python.py
@@ -1,12 +1,13 @@
 #It is a code that works with Flask.
 
-from flask import Flask
+from flask import Flask, render_template 
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "site actived"
+    
+    return render_template('your html files') 
 
 if __name__ == "__main__":
     app.run(debug=True)
